5e4e444276658fa20123c93b8e9c9fe9e7e1d394
chrisorwa <chrisorwa@gmail.com>
2015-04-09 18:23:05 +0300

    Update settings.R

--- settings.R
+++ settings.R
@@ -6,12 +6,6 @@ Version   : v2.0
 License   : Apache License, Version 2.0
 '
 
-# set working directory
-setwd('/Users/hp/Desktop/slicer/')
-
-# define global constants
-kMaxLevenshteinDistance <- 20
-
 # setup sqlite database
 db.drv <- dbDriver('SQLite')
 db.conn <- dbConnect(db.drv,'twitter2.sqlite')
